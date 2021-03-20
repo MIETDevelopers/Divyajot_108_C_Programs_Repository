b4430cbaa8bd09d22297cb0bb9ceeb9af286aa41
Divya kour <2020a1r108@mietjammu.in>
2021-03-20 12:45:55 +0530

    helloltration file

---
+++ 8_helloltration.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a = 1;
+    while ( a <= 10 ) //This line will allow us to print hello world 10 times.
+    {
+        printf ( "Hello World\n" );//Here we entered the texted to be printed.
+        a ++;
+    }
+    return 0;
+}
