7b91e315d1d94a700672e15150ec7291d6a9bdad
Divya kour <2020a1r108@mietjammu.in>
2021-03-31 22:03:01 +0530

    Do while file

---
+++ Dowhile.c
@@ -0,0 +1,12 @@
+//Divyajot
+//DO While program
+#include<stdio.h> // header function
+int main(){ //main function
+	int i=1; //intialize and Declare of i
+	do{ 
+	printf("Coding\n"); //printing output
+	i+=4; //increment by 4
+	}
+	while(i<9); //while checking condition 
+	return 0; //return statement
+}
