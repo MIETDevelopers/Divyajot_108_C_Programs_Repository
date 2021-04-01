1cda5cc56692e6e6457e738d762d1587f7005e81
Divya kour <2020a1r108@mietjammu.in>
2021-04-01 16:27:39 +0530

    HelloWorld file

---
+++ 38_HelloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>//Including standard input output header files from library.
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello World!");//here we have entered the text to be printed.
+   return 0;//returning.
+}
